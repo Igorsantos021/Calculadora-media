eff16b09ac66ae948a8e64daaebf5bcf8825d304
Igorsantos021 <santosigor868@gmail.com>
2023-09-05 21:15:27 -0300

    Envio de arquivos

---
+++ app.js
@@ -0,0 +1,11 @@
+//alert("BEM VINDO👽") //comando de saida de informação 
+
+/*
+vou fazer 
+varios comentarios
+de linhas*/
+console.log("Bem Vindo🎭")
+console.log("a media é:")
+console.log((8+7+9+2)/4)
+
+
